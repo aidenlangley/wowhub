be220de68c75271edc2cbf369cfb65967c70854e
nedia <me@aidenlangley.com>
2021-08-26 14:45:51 +1200

    Scrapping business card idea for the time being

--- src/lib/content/BusinessCard.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import type { Employee } from './business-card';
-
-  export let businessCard: Employee;
-</script>
-
-<div class="bg-black text-white">
-  <span>{businessCard.name}</span>
-</div>
