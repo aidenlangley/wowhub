22d50264e4c7f8d8ba2144144c7c1b5dcb20d8c8
nedia <me@aidenlangley.com>
2021-09-02 00:26:11 +1200

    🚀 deploy docs!

--- docs/_app/pages/news.svelte-eab13fae.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/vendor-49d51924.js";import"../chunks/seo-8585edea.js";export{N as default,b as load,a as loadDocs,l as loadNews}from"../chunks/news-d8e1f323.js";import"../chunks/singletons-ff603286.js";
